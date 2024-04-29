e59bde46f85902627cc78761c477bc54c721b150
Dani Anggara <47098245+danianggara@users.noreply.github.com>
2024-04-29 10:29:37 +0700

    init

---
+++ CustomDropdown/CustomDropdown/CustomDropdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDropdownApp.swift
+//  CustomDropdown
+//
+//  Created by Dani Anggara on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomDropdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
