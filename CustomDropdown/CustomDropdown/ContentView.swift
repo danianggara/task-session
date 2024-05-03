6a99133e47d2c9e01f3e9ded2c0d7b775655f124
Dani Anggara <47098245+danianggara@users.noreply.github.com>
2024-05-03 15:55:02 +0700

    add color extension

--- CustomDropdown/CustomDropdown/ContentView.swift
+++ CustomDropdown/CustomDropdown/ContentView.swift
@@ -56,7 +56,7 @@ struct ContentView: View {
             .frame(maxHeight: .infinity, alignment: .top)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.white.opacity(0.9))
+        .background(Color.baseGray)
         .navigationBarBackButtonHidden()
     }
     
@@ -385,6 +385,36 @@ extension View {
     }
 }
 
+extension Color {
+    static let baseGray = Color.init(hex: "#BCBCBC")
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
 
 
 #Preview {
